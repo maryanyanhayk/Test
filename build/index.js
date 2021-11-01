3ded7816b97d3005583a8ec8d588cbec446cb0f6
hayk.maryanyan <hayk.maryanyan@sfl.am>
2021-11-01 15:23:37 +0400

    added app part 1

--- build/index.js
+++ build/index.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("Hello from type script");
+var BakeryItemType;
+(function (BakeryItemType) {
+    BakeryItemType[BakeryItemType["Cake"] = 0] = "Cake";
+    BakeryItemType[BakeryItemType["Bread"] = 1] = "Bread";
+    BakeryItemType[BakeryItemType["Other"] = 2] = "Other";
+})(BakeryItemType || (BakeryItemType = {}));
+var donut = {
+    name: "Donut",
+    description: 'This donut has really vibrant pink frosting on it which makes it taste better!',
+    numberInStock: 10,
+    type: BakeryItemType.Cake,
+    price: 10,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello from type script");
+enum BakeryItemType{
+    Cake,
+    Bread,
+    Other,
+}
+
+interface BakeryItemPrototype{
+    name: string;
+    description?: string;
+    imageURL?: string;
+    numberInStock: number;
+    type: BakeryItemType;
+    price: number;
+}
+
+const donut: BakeryItemPrototype = {
+    name: "Donut",    
+    description: 'This donut has really vibrant pink frosting on it which makes it taste better!',
+    numberInStock: 10,
+    type: BakeryItemType.Cake,
+    price: 10,
+};
